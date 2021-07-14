9620aba257be05e759ae36b9bf9209dd44e10b3a
Amoury Damien <damien.amoury@laposte.net>
2021-07-14 14:48:46 +0200

    Ajout de la fonctionnalité d'export des données

--- src/app/components/abstract.component.ts
+++ src/app/components/abstract.component.ts
@@ -1,21 +1,13 @@
-import { OnDestroy, ViewChild } from '@angular/core';
+import { OnDestroy } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { blur, waitUntil } from '../app.utils';
 
-export abstract class AbstractComponent<T> implements OnDestroy {
+export abstract class AbstractComponent implements OnDestroy {
 
     private destroy: Subject<void> = new Subject<void>();
     protected destroy$: Observable<void> = this.destroy.asObservable();
 
-    @ViewChild('refresh', { static: true }) el: T;
-    
     ngOnDestroy() {
         this.destroy.next();
     }
     
-    refreshView(): void {
-        waitUntil(blur(this.element()), () => !!this.element(), 250);
-    }
-
-    protected abstract element(): HTMLElement;
 }
